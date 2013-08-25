f2ab1d5b0a9b6a66d40677a4cf535fd1d4e68195
Chris Pettitt <chris@samsarin.com>
2013-08-24 20:38:38 -0700

    Add topsort algorithm

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 exports.Graph = require("./lib/Graph");
+exports.alg = require("./lib/alg");
 exports.version = require("./lib/version");
---
+++ test/alg/topsort-test.js
@@ -0,0 +1,26 @@
+var assert = require("chai").assert,
+    Graph = require("../../lib/Graph"),
+    topsort = require("../../lib/alg/topsort");
+
+describe("alg.topsort", function() {
+  it("sorts nodes such that earlier nodes have directed edges to later nodes", function() {
+    var g = new Graph();
+    g.addNode("a");
+    g.addNode("b");
+    g.addNode("c");
+    g.addEdge(null, "b", "c");
+    g.addEdge(null, "c", "a");
+    assert.deepEqual(topsort(g), ["b", "c", "a"]);
+  });
+
+  it("throws CycleException if there is a cycle", function() {
+    var g = new Graph();
+    g.addNode("a");
+    g.addNode("b");
+    g.addNode("c");
+    g.addEdge(null, "b", "c");
+    g.addEdge(null, "c", "a");
+    g.addEdge(null, "a", "b");
+    assert.throws(function() { topsort(g); }, topsort.CycleException);
+  });
+});
