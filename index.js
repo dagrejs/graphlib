9bb9a33c4cbc651faa01badc56d295290a7541b1
Chris Pettitt <chris@samsarin.com>
2013-08-31 12:55:22 -0700

    Remove lib/alg.js

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 exports.Digraph = require("./lib/Digraph");
-exports.alg = require("./lib/alg");
+exports.alg = {
+  isAcyclic: require("./lib/alg/isAcyclic"),
+  findCycles: require("./lib/alg/findCycles"),
+  tarjan: require("./lib/alg/tarjan"),
+  topsort: require("./lib/alg/topsort")
+};
 exports.data = {
   PriorityQueue: require("./lib/data/PriorityQueue")
 };
