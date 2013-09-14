1ce39f13595afc3a893b8567f1888837baa44505
Chris Pettitt <chris@samsarin.com>
2013-09-13 19:32:23 -0700

    Test consolidation and bug fix in compoundify

--- test/CDigraph-test.js
+++ test/CDigraph-test.js
@@ -2,5 +2,6 @@ var CDigraph = require("..").CDigraph,
     Digraph = require("..").Digraph;
 
 describe("CDigraph", function() {
+  require("./abstract-Digraph-test")(CDigraph);
   require("./abstract-compoundify-test.js")("CDigraph", CDigraph, "Digraph", Digraph);
 });
--- test/CGraph-test.js
+++ test/CGraph-test.js
@@ -2,5 +2,6 @@ var CGraph = require("..").CGraph,
     Graph = require("..").Graph;
 
 describe("CGraph", function() {
+  require("./abstract-Graph-test")(CGraph);
   require("./abstract-compoundify-test.js")("CGraph", CGraph, "Graph", Graph);
 });
