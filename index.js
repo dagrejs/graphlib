f69d6ca097f7fa8505b9f4cf36acfc4a6e4d57c1
Chris Pettitt <cpettitt@gmail.com>
2014-08-09 20:27:04 -0700

    Some code style cleanup

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
-exports.Graph = require("./lib/Graph");
-exports.Digraph = require("./lib/Digraph");
-exports.CGraph = require("./lib/CGraph");
-exports.CDigraph = require("./lib/CDigraph");
-require("./lib/graph-converters");
+exports.Graph = require('./lib/Graph');
+exports.Digraph = require('./lib/Digraph');
+exports.CGraph = require('./lib/CGraph');
+exports.CDigraph = require('./lib/CDigraph');
+require('./lib/graph-converters');
 
 exports.alg = {
-  isAcyclic: require("./lib/alg/isAcyclic"),
-  components: require("./lib/alg/components"),
-  dijkstra: require("./lib/alg/dijkstra"),
-  dijkstraAll: require("./lib/alg/dijkstraAll"),
-  findCycles: require("./lib/alg/findCycles"),
-  floydWarshall: require("./lib/alg/floydWarshall"),
-  postorder: require("./lib/alg/postorder"),
-  preorder: require("./lib/alg/preorder"),
-  prim: require("./lib/alg/prim"),
-  tarjan: require("./lib/alg/tarjan"),
-  topsort: require("./lib/alg/topsort")
+  isAcyclic: require('./lib/alg/isAcyclic'),
+  components: require('./lib/alg/components'),
+  dijkstra: require('./lib/alg/dijkstra'),
+  dijkstraAll: require('./lib/alg/dijkstraAll'),
+  findCycles: require('./lib/alg/findCycles'),
+  floydWarshall: require('./lib/alg/floydWarshall'),
+  postorder: require('./lib/alg/postorder'),
+  preorder: require('./lib/alg/preorder'),
+  prim: require('./lib/alg/prim'),
+  tarjan: require('./lib/alg/tarjan'),
+  topsort: require('./lib/alg/topsort')
 };
 
 exports.converter = {
-  json: require("./lib/converter/json.js")
+  json: require('./lib/converter/json.js')
 };
 
-var filter = require("./lib/filter");
+var filter = require('./lib/filter');
 exports.filter = {
   all: filter.all,
   nodesFromList: filter.nodesFromList
 };
 
-exports.version = require("./lib/version");
+exports.version = require('./lib/version');
