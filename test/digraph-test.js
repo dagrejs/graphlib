d73f3c1882f616c4cc6b5f211ac1249b498b9336
Chris Pettitt <cpettitt@gmail.com>
2014-08-18 18:21:09 -0700

    Add some graph query functions

--- test/digraph-test.js
+++ test/digraph-test.js
@@ -19,159 +19,260 @@ describe("Digraph", function() {
     });
   });
 
-  describe("set", function() {
-    it("creates the node if it isn't part of the graph", function() {
-      g.set("key", "label");
-      expectSingleNodeGraph(g, "key", "label");
-    });
+  describe("node", function() {
+    describe("mutation", function() {
+      describe("set", function() {
+        it("creates the node if it isn't part of the graph", function() {
+          g.set("key", "label");
+          expectSingleNodeGraph(g, "key", "label");
+        });
 
-    it("replaces the node's value if it is part of the graph", function() {
-      g.set("key", "old");
-      g.set("key", "new");
-      expectSingleNodeGraph(g, "key", "new");
-    });
+        it("replaces the node's value if it is part of the graph", function() {
+          g.set("key", "old");
+          g.set("key", "new");
+          expectSingleNodeGraph(g, "key", "new");
+        });
 
-    it("sets the label to undefined if called with one arg", function() {
-      g.set("key");
-      expect(g.get("key")).to.be.undefined;
-    });
+        it("sets the label to undefined if called with one arg", function() {
+          g.set("key");
+          expect(g.get("key")).to.be.undefined;
+        });
 
-    it("is chainable", function() {
-      var g2 = g.set("key", "label");
-      expect(g).to.equal(g2);
+        it("is chainable", function() {
+          var g2 = g.set("key", "label");
+          expect(g).to.equal(g2);
+        });
+      });
+
+      describe("update", function() {
+        var updater = function(prev) { return prev + "-new"; };
+
+        it("creates the node if it isn't part of the graph", function() {
+          g.update("key", updater);
+          expectSingleNodeGraph(g, "key", undefined + "-new");
+        });
+
+        it("replaces the node's label if the node is part of the graph", function() {
+          g.set("key", "label");
+          g.update("key", updater);
+          expectSingleNodeGraph(g, "key", "label-new");
+        });
+
+        it("is chainable", function() {
+          var g2 = g.update("key", updater);
+          expect(g).to.equal(g2);
+        });
+      });
+
+      describe("delete", function() {
+        it("does nothing if the node is not part of the graph", function() {
+          g.delete("key");
+          expectEmptyGraph(g);
+        });
+
+        it("deletes the node if it is part of the graph", function() {
+          g.set("key", "label");
+          g.delete("key");
+          expectEmptyGraph(g);
+        });
+
+        it("deletes any in edges", function() {
+          g.setEdge("n1", "n2");
+          g.delete("n2");
+          expect(g.hasEdge("n1", "n2")).to.be.false;
+          expect(g.numEdges()).to.equal(0);
+          expectSingleNodeGraph(g, "n1", undefined);
+        });
+
+        it("deletes any out edges", function() {
+          g.setEdge("n1", "n2");
+          g.delete("n1");
+          expect(g.hasEdge("n1", "n2")).to.be.false;
+          expect(g.numEdges()).to.equal(0);
+          expectSingleNodeGraph(g, "n2", undefined);
+        });
+
+        it("decrements edge count once when deleting a self-loop", function() {
+          g.setEdge("n1", "n1");
+          g.delete("n1");
+          expect(g.numEdges()).to.equal(0);
+        });
+
+        it("is chainable", function() {
+          var g2 = g.delete("key");
+          expect(g).to.equal(g2);
+        });
+      });
+
+      describe("query", function() {
+        describe("successors", function() {
+          it("returns the successors of a node", function() {
+            g.setEdge("n1", "n1");
+            g.setEdge("n1", "n2");
+            g.setEdge("n2", "n3");
+            g.setEdge("n3", "n1");
+            expect(g.successors("n1").sort()).to.eql(["n1", "n2"]);
+          });
+
+          it("returns undefined if the node is not in the graph", function() {
+            expect(g.successors("n1")).to.be.undefined;
+          });
+        });
+
+        describe("predecessors", function() {
+          it("returns the predecessors of a node", function() {
+            g.setEdge("n1", "n1");
+            g.setEdge("n1", "n2");
+            g.setEdge("n2", "n3");
+            g.setEdge("n3", "n1");
+            expect(g.predecessors("n1").sort()).to.eql(["n1", "n3"]);
+          });
+
+          it("returns undefined if the node is not in the graph", function() {
+            expect(g.predecessors("n1")).to.be.undefined;
+          });
+        });
+
+        describe("neighbors", function() {
+          it("returns the neighbors of a node", function() {
+            g.setEdge("n1", "n1");
+            g.setEdge("n1", "n2");
+            g.setEdge("n2", "n3");
+            g.setEdge("n3", "n1");
+            expect(g.neighbors("n1").sort()).to.eql(["n1", "n2", "n3"]);
+          });
+
+          it("returns undefined if the node is not in the graph", function() {
+            expect(g.neighbors("n1")).to.be.undefined;
+          });
+        });
+
+        describe("sources", function() {
+          it("returns the nodes in the graph with no in edges", function() {
+            g.setEdge("n1", "n2");
+            g.setEdge("n2", "n3");
+            g.setEdge("n4", "n3");
+            g.set("n5");
+            expect(g.sources().sort()).to.eql(["n1", "n4", "n5"]);
+          });
+        });
+
+        describe("sinks", function() {
+          it("returns the nodes in the graph with no out edges", function() {
+            g.setEdge("n1", "n2");
+            g.setEdge("n3", "n2");
+            g.setEdge("n3", "n4");
+            g.set("n5");
+            expect(g.sinks().sort()).to.eql(["n2", "n4", "n5"]);
+          });
+        });
+      });
     });
   });
 
-  describe("update", function() {
-    var updater = function(prev) { return prev + "-new"; };
+  describe("edge", function() {
+    describe("mutation", function() {
+      describe("setEdge", function() {
+        it("creates the edge if it does not exist", function() {
+          g.set("n1");
+          g.set("n2");
+          g.setEdge("n1", "n2", "label");
+          expectSingleEdgeGraph(g, "n1", "n2", "label");
+        });
 
-    it("creates the node if it isn't part of the graph", function() {
-      g.update("key", updater);
-      expectSingleNodeGraph(g, "key", undefined + "-new");
+        it("creates the incident nodes if they don't exist", function() {
+          g.setEdge("n1", "n2", "label");
+          expect(g.has("n1")).to.be.true;
+          expect(g.has("n2")).to.be.true;
+          expectSingleEdgeGraph(g, "n1", "n2", "label");
+        });
+
+        it("replaces the edge's label if the edge is part of the graph", function() {
+          g.setEdge("n1", "n2", "old");
+          g.setEdge("n1", "n2", "new");
+          expectSingleEdgeGraph(g, "n1", "n2", "new");
+        });
+
+        it("is chainable", function() {
+          var g2 = g.setEdge("n1", "n2");
+          expect(g).to.equal(g2);
+        });
+      });
+
+      describe("updateEdge", function() {
+        var updater = function(label) { return label + "-new"; };
+
+        it("adds and updates the edge if it does not exist", function() {
+          g.updateEdge("n1", "n2", updater);
+          expectSingleEdgeGraph(g, "n1", "n2", undefined + "-new");
+        });
+
+        it("updates the edge's label if it is in the graph", function() {
+          g.setEdge("n1", "n2", "label");
+          g.updateEdge("n1", "n2", updater);
+          expectSingleEdgeGraph(g, "n1", "n2", "label-new");
+        });
+
+        it("is chainable", function() {
+          var g2 = g.updateEdge("n1", "n2", updater);
+          expect(g).to.equal(g2);
+        });
+      });
+
+      describe("deleteEdge", function() {
+        it("does nothing if the edge is not in the graph", function() {
+          g.deleteEdge("n1", "n2");
+          expectEmptyGraph(g);
+        });
+
+        it("removes the edge if it is in the graph", function() {
+          g.setEdge("n1", "n2");
+          g.deleteEdge("n1", "n2");
+          expect(g.hasEdge("n1", "n2")).to.be.false;
+          expect(g.numEdges()).to.equal(0);
+        });
+
+        it("is chainable", function() {
+          var g2 = g.deleteEdge("n1", "n2");
+          expect(g).to.equal(g2);
+        });
+      });
     });
 
-    it("replaces the node's label if the node is part of the graph", function() {
-      g.set("key", "label");
-      g.update("key", updater);
-      expectSingleNodeGraph(g, "key", "label-new");
-    });
+    describe("query", function() {
+      describe("inEdges", function() {
+        it("returns the edges that point at the specified node", function() {
+          g.setEdge("n1", "n2", "n1n2");
+          g.setEdge("n3", "n2", "n3n2");
+          g.setEdge("n3", "n4");
+          var edges = g.inEdges("n2").sort(function(e1, e2) {
+            if (e1.v < e2.v) return -1;
+            if (e1.v > e2.v) return 1;
+            return 0;
+          });
 
-    it("is chainable", function() {
-      var g2 = g.update("key", updater);
-      expect(g).to.equal(g2);
-    });
-  });
+          expect(edges).to.eql([{ v: "n1", w: "n2", label: "n1n2" },
+                                { v: "n3", w: "n2", label: "n3n2" }]);
+        });
 
-  describe("delete", function() {
-    it("does nothing if the node is not part of the graph", function() {
-      g.delete("key");
-      expectEmptyGraph(g);
-    });
+        it("returns undefined if the node is not in the graph", function() {
+          expect(g.inEdges("n1")).to.be.undefined;
+        });
 
-    it("deletes the node if it is part of the graph", function() {
-      g.set("key", "label");
-      g.delete("key");
-      expectEmptyGraph(g);
-    });
-
-    it("deletes any in edges", function() {
-      g.setEdge("n1", "n2");
-      g.delete("n2");
-      expect(g.hasEdge("n1", "n2")).to.be.false;
-      expect(g.numEdges()).to.equal(0);
-      expectSingleNodeGraph(g, "n1", undefined);
-    });
-
-    it("deletes any out edges", function() {
-      g.setEdge("n1", "n2");
-      g.delete("n1");
-      expect(g.hasEdge("n1", "n2")).to.be.false;
-      expect(g.numEdges()).to.equal(0);
-      expectSingleNodeGraph(g, "n2", undefined);
-    });
-
-    it("decrements edge count once when deleting a self-loop", function() {
-      g.setEdge("n1", "n1");
-      g.delete("n1");
-      expect(g.numEdges()).to.equal(0);
-    });
-
-    it("is chainable", function() {
-      var g2 = g.delete("key");
-      expect(g).to.equal(g2);
-    });
-  });
-
-  describe("setEdge", function() {
-    it("creates the edge if it does not exist", function() {
-      g.set("n1");
-      g.set("n2");
-      g.setEdge("n1", "n2", "label");
-      expectSingleEdgeGraph(g, "n1", "n2", "label");
-    });
-
-    it("creates the incident nodes if they don't exist", function() {
-      g.setEdge("n1", "n2", "label");
-      expect(g.has("n1")).to.be.true;
-      expect(g.has("n2")).to.be.true;
-      expectSingleEdgeGraph(g, "n1", "n2", "label");
-    });
-
-    it("replaces the edge's label if the edge is part of the graph", function() {
-      g.setEdge("n1", "n2", "old");
-      g.setEdge("n1", "n2", "new");
-      expectSingleEdgeGraph(g, "n1", "n2", "new");
-    });
-
-    it("is chainable", function() {
-      var g2 = g.setEdge("n1", "n2");
-      expect(g).to.equal(g2);
-    });
-  });
-
-  describe("updateEdge", function() {
-    var updater = function(label) { return label + "-new"; };
-
-    it("adds and updates the edge if it does not exist", function() {
-      g.updateEdge("n1", "n2", updater);
-      expectSingleEdgeGraph(g, "n1", "n2", undefined + "-new");
-    });
-
-    it("updates the edge's label if it is in the graph", function() {
-      g.setEdge("n1", "n2", "label");
-      g.updateEdge("n1", "n2", updater);
-      expectSingleEdgeGraph(g, "n1", "n2", "label-new");
-    });
-
-    it("is chainable", function() {
-      var g2 = g.updateEdge("n1", "n2", updater);
-      expect(g).to.equal(g2);
-    });
-  });
-
-  describe("deleteEdge", function() {
-    it("does nothing if the edge is not in the graph", function() {
-      g.deleteEdge("n1", "n2");
-      expectEmptyGraph(g);
-    });
-
-    it("removes the edge if it is in the graph", function() {
-      g.setEdge("n1", "n2");
-      g.deleteEdge("n1", "n2");
-      expect(g.hasEdge("n1", "n2")).to.be.false;
-      expect(g.numEdges()).to.equal(0);
-    });
-
-    it("is chainable", function() {
-      var g2 = g.deleteEdge("n1", "n2");
-      expect(g).to.equal(g2);
+        it("does not allow changes to the graph through the returned edge object", function() {
+          g.setEdge("n1", "n2", "label");
+          var edge = g.inEdges("n2")[0];
+          edge.label = "foo";
+          expect(g.getEdge("n1", "n2")).to.equal("label");
+        });
+      });
     });
   });
 });
 
 function expectEmptyGraph(g) {
-  expect(g.numNodes()).to.equal(0);
   expect(g.nodeIds()).to.be.empty;
+  expect(g.numNodes()).to.equal(0);
   expect(g.edges()).to.be.empty;
   expect(g.numEdges()).to.equal(0);
 }
