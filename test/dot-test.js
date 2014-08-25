f8594ce1409f48b1007088bf46c6448f8068c8f4
Chris Pettitt <cpettitt@gmail.com>
2014-08-24 21:54:52 -0700

    Add graphviz writer

--- test/dot-test.js
+++ test/dot-test.js
@@ -1,6 +1,9 @@
 var _ = require("lodash"),
     expect = require("./chai").expect,
-    dot = require("..").dot;
+    dot = require("..").dot,
+    Digraph = require("..").Digraph,
+    CDigraph = require("..").CDigraph,
+    Graph = require("..").Graph;
 
 describe("dot", function() {
   describe("read", function() {
@@ -371,6 +374,126 @@ describe("dot", function() {
       expect(gs).to.have.length(2);
       expect(gs[0].isDirected()).to.be.true;
       expect(gs[1].isDirected()).to.be.false;
+    });
+  });
+
+  describe("write", function() {
+    it("can write an empty digraph", function() {
+      var str = dot.write(new Digraph());
+      var g = dot.read(str);
+      expect(g.nodeCount()).to.equal(0);
+      expect(g.edgeCount()).to.equal(0);
+      expect(g.getGraph()).to.eql({});
+      expect(g.isDirected()).to.be.true;
+    });
+
+    it("can write an empty graph", function() {
+      var str = dot.write(new Graph());
+      var g = dot.read(str);
+      expect(g.nodeCount()).to.equal(0);
+      expect(g.edgeCount()).to.equal(0);
+      expect(g.getGraph()).to.eql({});
+      expect(g.isDirected()).to.be.false;
+    });
+
+    it("can write a graph label with an object", function() {
+      var g = new Digraph();
+      g.setGraph({ foo: "bar" });
+      var str = dot.write(g);
+      var g2 = dot.read(str);
+      expect(g2.getGraph()).to.eql({ foo: "bar" });
+    });
+
+    it("can write a node", function() {
+      var g = new Digraph();
+      g.setNode("n1");
+      var str = dot.write(g);
+      var g2 = dot.read(str);
+      expect(g2.hasNode("n1")).to.be.true;
+      expect(g2.getNode("n1")).to.eql({});
+      expect(g2.nodeCount()).to.equal(1);
+      expect(g2.edgeCount()).to.equal(0);
+    });
+
+    it("can write a node with attributes", function() {
+      var g = new Digraph();
+      g.setNode("n1", { foo: "bar" });
+      var str = dot.write(g);
+      var g2 = dot.read(str);
+      expect(g2.getNode("n1")).to.eql({ foo: "bar" });
+      expect(g2.nodeCount()).to.equal(1);
+      expect(g2.edgeCount()).to.equal(0);
+    });
+
+    it("can write an edge", function() {
+      var g = new Digraph();
+      g.setEdge("n1", "n2");
+      var str = dot.write(g, { strict: true });
+      var g2 = dot.read(str);
+      expect(g2.hasEdge("n1", "n2")).to.be.true;
+      expect(g2.getEdge("n1", "n2")).to.eql({});
+      expect(g2.nodeCount()).to.equal(2);
+      expect(g2.edgeCount()).to.equal(1);
+    });
+
+    it("can write an edge with attributes", function() {
+      var g = new Digraph();
+      g.setEdge("n1", "n2", { foo: "bar" });
+      var str = dot.write(g, { strict: true });
+      var g2 = dot.read(str);
+      expect(g2.getEdge("n1", "n2")).to.eql({ foo: "bar" });
+      expect(g2.nodeCount()).to.equal(2);
+      expect(g2.edgeCount()).to.equal(1);
+    });
+
+    it("can write multi-edges", function() {
+      var g = new Digraph();
+      g.setEdge("n1", "n2", [{ foo: "bar" }, { foo: "baz" }]);
+      var str = dot.write(g);
+      var g2 = dot.read(str);
+      expect(_.sortBy(g2.getEdge("n1", "n2"), "foo")).to.eql([
+        { foo: "bar" },
+        { foo: "baz" }
+      ]);
+      expect(g2.nodeCount()).to.equal(2);
+      expect(g2.edgeCount()).to.equal(1);
+    });
+
+    it("can write ids that must be escaped", function() {
+      var g = new Digraph();
+      g.setNode("\"n1\"");
+      var str = dot.write(g);
+      var g2 = dot.read(str);
+      expect(g2.hasNode("\"n1\"")).to.be.true;
+      expect(g2.getNode("\"n1\"")).to.eql({});
+      expect(g2.nodeCount()).to.equal(1);
+      expect(g2.edgeCount()).to.equal(0);
+    });
+
+    it("can write subgraphs", function() {
+      var g = new CDigraph();
+      g.setParent("n1", "root");
+      var str = dot.write(g);
+      var g2 = dot.read(str);
+      expect(g2.hasNode("n1")).to.be.true;
+      expect(g2.hasNode("root")).to.be.true;
+      expect(g2.getParent("n1")).to.equal("root");
+      expect(g2.nodeCount()).to.equal(2);
+      expect(g2.edgeCount()).to.equal(0);
+    });
+
+    it("can write subgraphs with attributes", function() {
+      var g = new CDigraph();
+      g.setParent("n1", "root");
+      g.setNode("root", { foo: "bar" });
+      var str = dot.write(g);
+      var g2 = dot.read(str);
+      expect(g2.hasNode("n1")).to.be.true;
+      expect(g2.hasNode("root")).to.be.true;
+      expect(g2.getNode("root")).to.eql({ foo: "bar" });
+      expect(g2.getParent("n1")).to.equal("root");
+      expect(g2.nodeCount()).to.equal(2);
+      expect(g2.edgeCount()).to.equal(0);
     });
   });
 });
