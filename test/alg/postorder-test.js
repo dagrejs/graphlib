b90f9bf7bb7d213a32b1069bdd3d5b177d7ee75e
Chris Pettitt <cpettitt@gmail.com>
2014-08-26 19:04:09 -0700

    Implement preorder and postorder in terms of DFS

--- test/alg/postorder-test.js
+++ test/alg/postorder-test.js
@@ -2,6 +2,7 @@ var _ = require("lodash"),
     expect = require("../chai").expect,
     Graph = require("../..").Graph,
     Digraph = require("../..").Digraph,
+    CDigraph = require("../..").CDigraph,
     postorder = require("../..").alg.postorder;
 
 describe("alg.postorder", function() {
@@ -11,25 +12,27 @@ describe("alg.postorder", function() {
     expect(postorder(g, "a")).to.eql(["a"]);
   });
 
+  it("visits each node in the graph once", function() {
+    var g = new Digraph();
+    g.setPath(["a", "b", "d", "e"]);
+    g.setPath(["a", "c", "d", "e"]);
+
+    var nodes = postorder(g, "a");
+    expect(_.sortBy(nodes)).to.eql(["a", "b", "c", "d", "e"]);
+  });
+
   it("works for an undirected tree", function() {
     var g = new Graph();
     g.setEdge("a", "b");
     g.setPath(["a", "c", "d"]);
     g.setEdge("c", "e");
 
-    var result = postorder(g, "a");
-    expect(_.sortBy(result)).to.eql(["a", "b", "c", "d", "e"]);
-    expect(result.indexOf("b")).to.be.lt(result.indexOf("a"));
-    expect(result.indexOf("c")).to.be.lt(result.indexOf("a"));
-    expect(result.indexOf("d")).to.be.lt(result.indexOf("c"));
-    expect(result.indexOf("e")).to.be.lt(result.indexOf("c"));
-  });
-
-  it("fails for an undirected graph that is not a tree", function() {
-    var g = new Graph();
-    g.setPath(["a", "b", "c", "a"]);
-
-    expect(function() { postorder(g, "a"); }).to.throw();
+    var nodes = postorder(g, "a");
+    expect(_.sortBy(nodes)).to.eql(["a", "b", "c", "d", "e"]);
+    expect(nodes.indexOf("b")).to.be.lt(nodes.indexOf("a"));
+    expect(nodes.indexOf("c")).to.be.lt(nodes.indexOf("a"));
+    expect(nodes.indexOf("d")).to.be.lt(nodes.indexOf("c"));
+    expect(nodes.indexOf("e")).to.be.lt(nodes.indexOf("c"));
   });
 
   it("works for a directed tree", function() {
@@ -38,18 +41,69 @@ describe("alg.postorder", function() {
     g.setPath(["a", "c", "d"]);
     g.setEdge("c", "e");
 
-    var result = postorder(g, "a");
-    expect(_.sortBy(result)).to.eql(["a", "b", "c", "d", "e"]);
-    expect(result.indexOf("b")).to.be.lt(result.indexOf("a"));
-    expect(result.indexOf("c")).to.be.lt(result.indexOf("a"));
-    expect(result.indexOf("d")).to.be.lt(result.indexOf("c"));
-    expect(result.indexOf("e")).to.be.lt(result.indexOf("c"));
+    var nodes = postorder(g, "a");
+    expect(_.sortBy(nodes)).to.eql(["a", "b", "c", "d", "e"]);
+    expect(nodes.indexOf("b")).to.be.lt(nodes.indexOf("a"));
+    expect(nodes.indexOf("c")).to.be.lt(nodes.indexOf("a"));
+    expect(nodes.indexOf("d")).to.be.lt(nodes.indexOf("c"));
+    expect(nodes.indexOf("e")).to.be.lt(nodes.indexOf("c"));
   });
 
-  it("fails for a directed graph that is not a tree", function() {
+  it("works for a directed diamond", function() {
     var g = new Digraph();
-    g.setPath(["a", "b", "a"]);
-    expect(function() { postorder(g, "a"); }).to.throw();
+    g.setPath(["a", "b", "d"]);
+    g.setPath(["a", "c", "d"]);
+
+    var nodes = postorder(g, "a");
+    expect(_.sortBy(nodes)).to.eql(["a", "b", "c", "d"]);
+    expect(nodes.indexOf("b")).to.be.lt(nodes.indexOf("a"));
+    expect(nodes.indexOf("c")).to.be.lt(nodes.indexOf("a"));
+    expect(nodes.indexOf("d")).to.be.lt(
+        Math.min(nodes.indexOf("b"), nodes.indexOf("c")));
+  });
+
+  it("finishes early if the callback returns false", function() {
+    var g = new Digraph();
+    g.setPath(["a", "b", "c", "d"]);
+
+    var nodes = postorder(g, "a", function(v) {
+      return v !== "c";
+    });
+
+    expect(_.sortBy(nodes)).to.eql(["a", "b"]);
+    expect(nodes.indexOf("b")).to.be.lt(nodes.indexOf("a"));
+  });
+
+  it("ends the branch if callback returns null", function() {
+    var g = new Digraph();
+    g.setEdge("a", "b");
+    g.setPath(["a", "c", "d"]);
+    g.setPath(["a", "f", "e"]);
+
+    var nodes = postorder(g, "a", function(v) {
+      return v !== "d" || null;
+    });
+
+    expect(_.sortBy(nodes)).to.eql(["a", "b", "c", "e", "f"]);
+    expect(nodes.indexOf("b")).to.be.lt(nodes.indexOf("a"));
+    expect(nodes.indexOf("c")).to.be.lt(nodes.indexOf("a"));
+    expect(nodes.indexOf("f")).to.be.lt(nodes.indexOf("a"));
+    expect(nodes.indexOf("e")).to.be.lt(nodes.indexOf("f"));
+  });
+
+  it("uses a supplied successors function", function() {
+    var g = new CDigraph();
+    g.setParent("a", "root");
+    g.setParent("a1", "a");
+    g.setParent("a2", "a");
+    g.setParent("b", "root");
+
+    var nodes = postorder(g, "root", undefined, g.getChildren.bind(g));
+    expect(_.sortBy(nodes)).to.eql(["a", "a1", "a2", "b", "root"]);
+    expect(nodes.indexOf("a")).to.be.lt(nodes.indexOf("root"));
+    expect(nodes.indexOf("a1")).to.be.lt(nodes.indexOf("a"));
+    expect(nodes.indexOf("a2")).to.be.lt(nodes.indexOf("a"));
+    expect(nodes.indexOf("b")).to.be.lt(nodes.indexOf("root"));
   });
 
   it("fails if root is not in the graph", function() {
--- test/alg/preorder-test.js
+++ test/alg/preorder-test.js
@@ -2,6 +2,7 @@ var _ = require("lodash"),
     expect = require("../chai").expect,
     Graph = require("../..").Graph,
     Digraph = require("../..").Digraph,
+    CDigraph = require("../..").CDigraph,
     preorder = require("../..").alg.preorder;
 
 describe("alg.preorder", function() {
@@ -11,25 +12,27 @@ describe("alg.preorder", function() {
     expect(preorder(g, "a")).to.eql(["a"]);
   });
 
+  it("visits each node in the graph once", function() {
+    var g = new Digraph();
+    g.setPath(["a", "b", "d", "e"]);
+    g.setPath(["a", "c", "d", "e"]);
+
+    var nodes = preorder(g, "a");
+    expect(_.sortBy(nodes)).to.eql(["a", "b", "c", "d", "e"]);
+  });
+
   it("works for an undirected tree", function() {
     var g = new Graph();
     g.setEdge("a", "b");
     g.setPath(["a", "c", "d"]);
     g.setEdge("c", "e");
 
-    var result = preorder(g, "a");
-    expect(_.sortBy(result)).to.eql(["a", "b", "c", "d", "e"]);
-    expect(result.indexOf("a")).to.be.lt(result.indexOf("b"));
-    expect(result.indexOf("a")).to.be.lt(result.indexOf("c"));
-    expect(result.indexOf("c")).to.be.lt(result.indexOf("d"));
-    expect(result.indexOf("c")).to.be.lt(result.indexOf("e"));
-  });
-
-  it("fails for an undirected graph that is not a tree", function() {
-    var g = new Graph();
-    g.setPath(["a", "b", "c", "a"]);
-
-    expect(function() { preorder(g, "a"); }).to.throw();
+    var nodes = preorder(g, "a");
+    expect(_.sortBy(nodes)).to.eql(["a", "b", "c", "d", "e"]);
+    expect(nodes.indexOf("b")).to.be.gt(nodes.indexOf("a"));
+    expect(nodes.indexOf("c")).to.be.gt(nodes.indexOf("a"));
+    expect(nodes.indexOf("d")).to.be.gt(nodes.indexOf("c"));
+    expect(nodes.indexOf("e")).to.be.gt(nodes.indexOf("c"));
   });
 
   it("works for a directed tree", function() {
@@ -38,18 +41,70 @@ describe("alg.preorder", function() {
     g.setPath(["a", "c", "d"]);
     g.setEdge("c", "e");
 
-    var result = preorder(g, "a");
-    expect(_.sortBy(result)).to.eql(["a", "b", "c", "d", "e"]);
-    expect(result.indexOf("a")).to.be.lt(result.indexOf("b"));
-    expect(result.indexOf("a")).to.be.lt(result.indexOf("c"));
-    expect(result.indexOf("c")).to.be.lt(result.indexOf("d"));
-    expect(result.indexOf("c")).to.be.lt(result.indexOf("e"));
+    var nodes = preorder(g, "a");
+    expect(_.sortBy(nodes)).to.eql(["a", "b", "c", "d", "e"]);
+    expect(nodes.indexOf("b")).to.be.gt(nodes.indexOf("a"));
+    expect(nodes.indexOf("c")).to.be.gt(nodes.indexOf("a"));
+    expect(nodes.indexOf("d")).to.be.gt(nodes.indexOf("c"));
+    expect(nodes.indexOf("e")).to.be.gt(nodes.indexOf("c"));
   });
 
-  it("fails for a directed graph that is not a tree", function() {
+  it("works for a directed diamond", function() {
     var g = new Digraph();
-    g.setPath(["a", "b", "a"]);
-    expect(function() { preorder(g, "a"); }).to.throw();
+    g.setPath(["a", "b", "d"]);
+    g.setPath(["a", "c", "d"]);
+
+    var nodes = preorder(g, "a");
+    expect(_.sortBy(nodes)).to.eql(["a", "b", "c", "d"]);
+    expect(nodes.indexOf("b")).to.be.gt(nodes.indexOf("a"));
+    expect(nodes.indexOf("c")).to.be.gt(nodes.indexOf("a"));
+    expect(nodes.indexOf("d")).to.be.gt(
+        Math.min(nodes.indexOf("b"), nodes.indexOf("c")));
+    expect(Math.max(nodes.indexOf("b"), nodes.indexOf("c"))).to.be.gt(
+        nodes.indexOf("d"));
+  });
+
+  it("finishes early if the callback returns false", function() {
+    var g = new Digraph();
+    g.setPath(["a", "b", "c", "d"]);
+
+    var nodes = preorder(g, "a", function(v) {
+      return v !== "c";
+    });
+
+    expect(_.sortBy(nodes)).to.eql(["a", "b"]);
+    expect(nodes.indexOf("b")).to.be.gt(nodes.indexOf("a"));
+  });
+
+  it("ends the branch if callback returns null", function() {
+    var g = new Digraph();
+    g.setEdge("a", "b");
+    g.setPath(["a", "c", "d"]);
+    g.setPath(["a", "f", "e"]);
+
+    var nodes = preorder(g, "a", function(v) {
+      return v !== "c" || null;
+    });
+
+    expect(_.sortBy(nodes)).to.eql(["a", "b", "e", "f"]);
+    expect(nodes.indexOf("b")).to.be.gt(nodes.indexOf("a"));
+    expect(nodes.indexOf("f")).to.be.gt(nodes.indexOf("a"));
+    expect(nodes.indexOf("e")).to.be.gt(nodes.indexOf("f"));
+  });
+
+  it("uses a supplied successors function", function() {
+    var g = new CDigraph();
+    g.setParent("a", "root");
+    g.setParent("a1", "a");
+    g.setParent("a2", "a");
+    g.setParent("b", "root");
+
+    var nodes = preorder(g, "root", undefined, g.getChildren.bind(g));
+    expect(_.sortBy(nodes)).to.eql(["a", "a1", "a2", "b", "root"]);
+    expect(nodes.indexOf("a")).to.be.gt(nodes.indexOf("root"));
+    expect(nodes.indexOf("a1")).to.be.gt(nodes.indexOf("a"));
+    expect(nodes.indexOf("a2")).to.be.gt(nodes.indexOf("a"));
+    expect(nodes.indexOf("b")).to.be.gt(nodes.indexOf("root"));
   });
 
   it("fails if root is not in the graph", function() {
