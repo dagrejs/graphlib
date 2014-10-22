35659d1746fd75b9d58bb4bf76b8ba99f7a42134
Chris Pettitt <cpettitt@gmail.com>
2014-10-22 06:28:33 -0700

    Prep for v1.0.0 release

--- dist/graphlib.core.js
+++ dist/graphlib.core.js
@@ -1170,6 +1170,6 @@ if (!lodash) {
 module.exports = lodash;
 
 },{"lodash":undefined}],21:[function(require,module,exports){
-module.exports = '0.9.1';
+module.exports = '1.0.0';
 
 },{}]},{},[1]);
