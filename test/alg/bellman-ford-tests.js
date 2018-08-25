fcce9e68cb9228795014f6087e5ba027512f9348
Miltiadis Stouras <m.stouras@gmail.com>
2018-08-25 17:01:25 +0300

    Create a function that runs the common tests of all shortest paths algorithms.

--- test/alg/bellman-ford-tests.js
+++ test/alg/bellman-ford-tests.js
@@ -1,84 +1,10 @@
-var expect = require("../chai").expect;
-
-var Graph = require("../..").Graph,
-    bellmanFord = require("../..").alg.bellmanFord;
+var expect = require("../chai").expect,
+    Graph = require("../..").Graph,
+    bellmanFord = require("../..").alg.bellmanFord,
+    shortestPathsTests = require("./shortest-paths-tests.js");
 
 describe("alg.bellmanFord", function(){
-  it("Assigns distance 0 for the source node", function() {
-    var g = new Graph();
-    g.setNode("source");
-    expect(bellmanFord(g, "source")).to.eql({ source: { distance: 0 } });
-  });
-
-  it("Returns Number.POSITIVE_INFINITY for unconnected nodes", function() {
-    var g = new Graph();
-    g.setNode("a");
-    g.setNode("b");
-    expect(bellmanFord(g, "a")).to.eql({
-      a: { distance: 0 },
-      b: { distance: Number.POSITIVE_INFINITY }
-    });
-  });
-
-  it("Returns the distance and predecessor for all nodes", function() {
-    var g = new Graph();
-    g.setPath(["a", "b", "c"]);
-    g.setEdge("b", "d");
-    expect(bellmanFord(g, "a")).to.eql({
-      a: { distance: 0 },
-      b: { distance: 1, predecessor: "a" },
-      c: { distance: 2, predecessor: "b" },
-      d: { distance: 2, predecessor: "b" }
-    });
-  });
-
-  it("Works for undirected graphs", function() {
-    var g = new Graph({ directed: false });
-    g.setPath(["a", "b", "c"]);
-    g.setEdge("b", "d");
-    expect(bellmanFord(g, "a")).to.eql({
-      a: { distance: 0 },
-      b: { distance: 1, predecessor: "a" },
-      c: { distance: 2, predecessor: "b" },
-      d: { distance: 2, predecessor: "b" }
-    });
-  });
-
-  it("Works with an optionally supplied weight function", function() {
-    var g = new Graph();
-    g.setEdge("a", "b", 1);
-    g.setEdge("a", "c", 2);
-    g.setEdge("b", "d", 3);
-    g.setEdge("c", "d", 3);
-
-    expect(bellmanFord(g, "a", weightFn(g))).to.eql({
-      a: { distance: 0 },
-      b: { distance: 1, predecessor: "a" },
-      c: { distance: 2, predecessor: "a" },
-      d: { distance: 4, predecessor: "b" }
-    });
-  });
-
-  it("Uses an optionally supplied edge function", function() {
-    var g = new Graph();
-    g.setPath(["a", "c", "d"]);
-    g.setEdge("b", "c");
-
-    function edgeFn(v){
-      switch (v) {
-        case "d": return [{ v: "d", w: "c" }];
-        case "c": return [{ v: "c", w: "b" }, { v: "c", w: "a" }];
-        default: return [];
-      }
-    }
-
-    expect(bellmanFord(g, "d", undefined, edgeFn)).to.eql({
-      a: { distance: 2, predecessor: "c" },
-      b: { distance: 2, predecessor: "c" },
-      c: { distance: 1, predecessor: "d" },
-      d: { distance: 0 }
-    });
-  });
+  shortestPathsTests(bellmanFord);
 
   it("Works with negative weight edges on the graph", function() {
     var g = new Graph();
