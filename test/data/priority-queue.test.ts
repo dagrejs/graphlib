ba9edc0ae451cc5c46f49d52cd1d519574c25df6
Chris West (Faux) <gpg@goeswhere.com>
2021-04-28 09:30:56 +0100

    chore: pq test is ts

--- test/data/priority-queue.test.ts
+++ test/data/priority-queue.test.ts
@@ -1,6 +1,5 @@
-const _ = require('lodash');
-const expect = require('../chai').expect;
-const PriorityQueue = require('../../dist/data/priority-queue');
+import _ from 'lodash';
+import PriorityQueue from '../../lib/data/priority-queue';
 
 describe('data.PriorityQueue', function () {
   let pq;
@@ -11,14 +10,14 @@ describe('data.PriorityQueue', function () {
 
   describe('size', function () {
     it('returns 0 for an empty queue', function () {
-      expect(pq.size()).to.equal(0);
+      expect(pq.size()).toEqual(0);
     });
 
     it('returns the number of elements in the queue', function () {
       pq.add('a', 1);
-      expect(pq.size()).to.equal(1);
+      expect(pq.size()).toEqual(1);
       pq.add('b', 2);
-      expect(pq.size()).to.equal(2);
+      expect(pq.size()).toEqual(2);
     });
   });
 
@@ -29,7 +28,7 @@ describe('data.PriorityQueue', function () {
       pq.add(false, 3);
       pq.add(undefined, 4);
       pq.add(null, 5);
-      expect(_.sortBy(pq.keys())).to.eql(
+      expect(_.sortBy(pq.keys())).toEqual(
         _.sortBy(['a', '1', 'false', 'undefined', 'null']),
       );
     });
@@ -38,11 +37,11 @@ describe('data.PriorityQueue', function () {
   describe('has', function () {
     it('returns true if the key is in the queue', function () {
       pq.add('a', 1);
-      expect(pq.has('a')).to.be.true;
+      expect(pq.has('a')).toBe(true);
     });
 
     it('returns false if the key is not in the queue', function () {
-      expect(pq.has('a')).to.be.false;
+      expect(pq.has('a')).toBe(false);
     });
   });
 
@@ -50,12 +49,12 @@ describe('data.PriorityQueue', function () {
     it('returns the current priority for the key', function () {
       pq.add('a', 1);
       pq.add('b', 2);
-      expect(pq.priority('a')).to.equal(1);
-      expect(pq.priority('b')).to.equal(2);
+      expect(pq.priority('a')).toEqual(1);
+      expect(pq.priority('b')).toEqual(2);
     });
 
     it('returns undefined if the key is not in the queue', function () {
-      expect(pq.priority('foo')).to.be.undefined;
+      expect(pq.priority('foo')).toBe(undefined);
     });
   });
 
@@ -63,36 +62,36 @@ describe('data.PriorityQueue', function () {
     it('throws an error if there is no element in the queue', function () {
       expect(function () {
         pq.min();
-      }).to.throw();
+      }).toThrow();
     });
 
     it('returns the smallest element', function () {
       pq.add('b', 2);
       pq.add('a', 1);
-      expect(pq.min()).to.equal('a');
+      expect(pq.min()).toEqual('a');
     });
 
     it('does not remove the minimum element from the queue', function () {
       pq.add('b', 2);
       pq.add('a', 1);
       pq.min();
-      expect(pq.size()).to.equal(2);
+      expect(pq.size()).toEqual(2);
     });
   });
 
   describe('add', function () {
     it('adds the key to the queue', function () {
       pq.add('a', 1);
-      expect(pq.keys()).to.eql(['a']);
+      expect(pq.keys()).toEqual(['a']);
     });
 
     it('returns true if the key was added', function () {
-      expect(pq.add('a', 1)).to.be.true;
+      expect(pq.add('a', 1)).toBe(true);
     });
 
     it('returns false if the key already exists in the queue', function () {
       pq.add('a', 1);
-      expect(pq.add('a', 1)).to.be.false;
+      expect(pq.add('a', 1)).toBe(false);
     });
   });
 
@@ -103,17 +102,17 @@ describe('data.PriorityQueue', function () {
       pq.add('c', 3);
       pq.add('e', 5);
       pq.add('d', 4);
-      expect(pq.removeMin()).to.equal('a');
-      expect(pq.removeMin()).to.equal('b');
-      expect(pq.removeMin()).to.equal('c');
-      expect(pq.removeMin()).to.equal('d');
-      expect(pq.removeMin()).to.equal('e');
+      expect(pq.removeMin()).toEqual('a');
+      expect(pq.removeMin()).toEqual('b');
+      expect(pq.removeMin()).toEqual('c');
+      expect(pq.removeMin()).toEqual('d');
+      expect(pq.removeMin()).toEqual('e');
     });
 
     it('throws an error if there is no element in the queue', function () {
       expect(function () {
         pq.removeMin();
-      }).to.throw();
+      }).toThrow();
     });
   });
 
@@ -121,20 +120,20 @@ describe('data.PriorityQueue', function () {
     it('decreases the priority of a key', function () {
       pq.add('a', 1);
       pq.decrease('a', -1);
-      expect(pq.priority('a')).to.equal(-1);
+      expect(pq.priority('a')).toEqual(-1);
     });
 
     it('raises an error if the key is not in the queue', function () {
       expect(function () {
         pq.decrease('a', -1);
-      }).to.throw();
+      }).toThrow();
     });
 
     it('raises an error if the new priority is greater than current', function () {
       pq.add('a', 1);
       expect(function () {
         pq.decrease('a', 2);
-      }).to.throw();
+      }).toThrow();
     });
   });
 });
