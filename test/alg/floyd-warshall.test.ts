7d3dbbb70091e934731e4e3320e8182c1b0e72a1
David Newell <davidnewell@google.com>
2022-07-25 13:11:56 +0000

    Migrating the first test to TypeScript

--- test/alg/floyd-warshall.test.ts
+++ test/alg/floyd-warshall.test.ts
@@ -1,7 +1,7 @@
-var expect = require("../chai").expect;
-var Graph = require("../..").Graph;
-var floydWarshall = require("../..").alg.floydWarshall;
-var allShortestPathsTest = require("./all-shortest-paths-test");
+import { expect } from "../chai";
+import { Graph } from "../../lib/graph";
+import { floydWarshall } from "../../lib/alg/floyd-warshall";
+import * as allShortestPathsTest from "./all-shortest-paths-test";
 
 describe("alg.floydWarshall", function() {
   allShortestPathsTest.tests(floydWarshall);
