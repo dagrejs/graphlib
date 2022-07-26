011024465a83dfdf3844ba98bff37df35b47cc46
David Newell <davidnewell@google.com>
2022-07-26 15:30:04 +0000

    Moving the version to typescript

--- test/version.test.ts
+++ test/version.test.ts
@@ -1,8 +1,9 @@
 import { expect } from "chai";
+import { version } from "../lib/version";
 
 describe('version', function() {
   it('should match the version from package.json', function() {
     var packageVersion = require('../package').version;
-    expect(require('../lib/version')).to.equal(packageVersion);
+    expect(version).to.equal(packageVersion);
   });
 });
