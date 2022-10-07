6d6efc3b36ec3cc36fbc5821f788b6b86a27ebec
David Newell <davidnewell@google.com>
2022-10-07 14:32:01 +0000

    Converting json and version tests

--- test/json.test.ts
+++ test/json.test.ts
@@ -1,60 +1,59 @@
 import { Graph } from "../lib/graph";
-import { expect } from "chai";
 import { read, write } from "../lib/json";
 
 describe("json", function() {
   it("preserves the graph options", function() {
-    expect(rw(new Graph({ directed: true })).isDirected()).to.be.true;
-    expect(rw(new Graph({ directed: false })).isDirected()).to.be.false;
-    expect(rw(new Graph({ multigraph: true })).isMultigraph()).to.be.true;
-    expect(rw(new Graph({ multigraph: false })).isMultigraph()).to.be.false;
-    expect(rw(new Graph({ compound: true })).isCompound()).to.be.true;
-    expect(rw(new Graph({ compound: false })).isCompound()).to.be.false;
+    expect(rw(new Graph({ directed: true })).isDirected()).toBe(true);
+    expect(rw(new Graph({ directed: false })).isDirected()).toBe(false);
+    expect(rw(new Graph({ multigraph: true })).isMultigraph()).toBe(true);
+    expect(rw(new Graph({ multigraph: false })).isMultigraph()).toBe(false);
+    expect(rw(new Graph({ compound: true })).isCompound()).toBe(true);
+    expect(rw(new Graph({ compound: false })).isCompound()).toBe(false);
   });
 
   it("preserves the graph value, if any", function() {
-    expect(rw(new Graph().setGraph(1)).graph()).equals(1);
-    expect(rw(new Graph().setGraph({ foo: "bar" })).graph()).eqls({ foo: "bar" });
-    expect(rw(new Graph()).graph()).to.be.undefined;
+    expect(rw(new Graph().setGraph(1)).graph()).toEqual(1);
+    expect(rw(new Graph().setGraph({ foo: "bar" })).graph()).toEqual({ foo: "bar" });
+    expect(rw(new Graph()).graph()).toBeUndefined;
   });
 
   it("preserves nodes", function() {
-    expect(rw(new Graph().setNode("a")).hasNode("a")).to.be.true;
-    expect(rw(new Graph().setNode("a")).node("a")).to.be.undefined;
-    expect(rw(new Graph().setNode("a", 1)).node("a")).equals(1);
+    expect(rw(new Graph().setNode("a")).hasNode("a")).toBe(true);
+    expect(rw(new Graph().setNode("a")).node("a")).toBeUndefined;
+    expect(rw(new Graph().setNode("a", 1)).node("a")).toEqual(1);
     expect(rw(new Graph().setNode("a", { foo: "bar" })).node("a"))
-      .eqls({ foo: "bar" });
+      .toEqual({ foo: "bar" });
   });
 
   it("preserves simple edges", function() {
-    expect(rw(new Graph().setEdge("a", "b")).hasEdge("a", "b")).to.be.true;
-    expect(rw(new Graph().setEdge("a", "b")).edge("a", "b")).to.be.undefined;
-    expect(rw(new Graph().setEdge("a", "b", 1)).edge("a", "b")).equals(1);
+    expect(rw(new Graph().setEdge("a", "b")).hasEdge("a", "b")).toBe(true);
+    expect(rw(new Graph().setEdge("a", "b")).edge("a", "b")).toBeUndefined;
+    expect(rw(new Graph().setEdge("a", "b", 1)).edge("a", "b")).toEqual(1);
     expect(rw(new Graph().setEdge("a", "b", { foo: "bar" })).edge("a", "b"))
-      .eqls({ foo: "bar" });
+      .toEqual({ foo: "bar" });
   });
 
   it("preserves multi-edges", function() {
     var g = new Graph({ multigraph: true });
 
     g.setEdge({ v: "a", w: "b", name: "foo" });
-    expect(rw(g).hasEdge("a", "b", "foo")).to.be.true;
+    expect(rw(g).hasEdge("a", "b", "foo")).toBe(true);
 
     g.setEdge({ v: "a", w: "b", name: "foo" });
-    expect(rw(g).edge("a", "b", "foo")).to.be.undefined;
+    expect(rw(g).edge("a", "b", "foo")).toBeUndefined;
 
     g.setEdge({ v: "a", w: "b", name: "foo" }, 1);
-    expect(rw(g).edge("a", "b", "foo")).equals(1);
+    expect(rw(g).edge("a", "b", "foo")).toEqual(1);
 
     g.setEdge({ v: "a", w: "b", name: "foo" }, { foo: "bar" });
-    expect(rw(g).edge("a", "b", "foo")).eqls({ foo: "bar" });
+    expect(rw(g).edge("a", "b", "foo")).toEqual({ foo: "bar" });
   });
 
   it("preserves parent / child relationships", function() {
     expect(rw(new Graph({ compound: true }).setNode("a")).parent("a"))
-      .to.be.undefined;
+      .toBeUndefined;
     expect(rw(new Graph({ compound: true }).setParent("a", "parent")).parent("a"))
-      .to.equal("parent");
+      .toEqual("parent");
   });
 });
 
--- test/version.test.ts
+++ test/version.test.ts
@@ -1,9 +1,8 @@
-import { expect } from "chai";
 import { version } from "../lib/version";
 
 describe('version', function() {
   it('should match the version from package.json', function() {
     var packageVersion = require('../package').version;
-    expect(version).to.equal(packageVersion);
+    expect(version).toEqual(packageVersion);
   });
 });
