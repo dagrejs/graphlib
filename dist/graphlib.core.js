2f9a5537493a40b925b46eb2a3f1d50ad79b1678
David Newell <davidnewell@google.com>
2024-03-12 14:58:59 +0000

    Bumping the package.json version

--- dist/graphlib.core.js
+++ dist/graphlib.core.js
@@ -1499,7 +1499,7 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.default = void 0;
-var _default = exports.default = '2.1.14-pre';
+var _default = exports.default = '2.2.0';
 module.exports = exports.default;
 
 },{}]},{},[1])(1)
